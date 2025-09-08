e8439574dcf3bc94cd5c2a8d7ca01b4053e42973
Gavrielfps <gabrieldeandradebernardo20@gmail.com>
2025-09-08 20:58:56 -0300

    Chamando função de somar
    O codigo agora chama a função de somar e imprime

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     }
 
     public static void main(String[] args){
-        System.out.println("Olá GitHub!");
+        int result = somar(5, 6);
+        System.out.println(result);
     }
 }
